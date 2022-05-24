6282dde29d8e63ea7da043069ca6918d48b8774d
Mostafizur rahman <sourav10681@gmail.com>
2022-05-24 21:45:17 +0600

    installation & connet with mongodb

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6ooih.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Computer Manufacturer parts')
+})
+
+app.listen(port, () => {
+    console.log('The computer manufacturer parts app listening on', port)
+})
