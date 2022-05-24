53c6cefd8fd6f53c2ca7e287b4166151ef8699d8
Mostafizur rahman <sourav10681@gmail.com>
2022-05-24 22:06:26 +0600

    find all parts api

--- index.js
+++ index.js
@@ -14,7 +14,27 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6ooih.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db('computerParts').collection('parts');
 
+        // get all parts
+        app.get('/part', async (req, res) => {
+            const query = {}
+            const cursor = partsCollection.find(query);
+            const parts = await cursor.toArray();
+            res.send(parts)
+        });
+
+
+    }
+
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from Computer Manufacturer parts')
