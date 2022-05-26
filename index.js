2fe70e762dac5aee06c680283449b29437163b0b
Mostafizur rahman <sourav10681@gmail.com>
2022-05-26 21:38:13 +0600

    payment & delete api creation

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
+// jwt
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
@@ -36,6 +37,7 @@ async function run() {
         const placeOrderCollection = client.db('computerParts').collection('placeOrder');
         const userCollection = client.db('computerParts').collection('users');
         const reviewCollection = client.db('computerParts').collection('reviews');
+        const paymentCollection = client.db('computerParts').collection('payments');
 
 
         // get all parts
@@ -59,14 +61,14 @@ async function run() {
             const users = await userCollection.find().toArray();
             res.send(users)
         })
-
+        // get admin
         app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
             const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
             res.send({ admin: isAdmin })
         })
-
+        // put a admin
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const requester = req.decoded.email;
@@ -84,7 +86,7 @@ async function run() {
             }
 
         })
-
+        // get user
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -97,12 +99,13 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30d' })
             res.send({ result, token });
         })
-
+        // get all order
         app.post("/placeorder", async (req, res) => {
             const placeOrder = req.body;
             const result = await placeOrderCollection.insertOne(placeOrder);
             res.send(result);
         });
+        // get all order with jst token
         app.get('/placeorder', verifyJWT, async (req, res) => {
             const customerEmail = req.query.email;
             // console.log(customerEmail);
@@ -117,12 +120,14 @@ async function run() {
             }
 
         })
+        // get all order
         app.get('/placeorder', async (req, res) => {
             const query = {};
             const cursor = placeOrderCollection.find(query);
             const orders = await cursor.toArray();
             res.send(orders);
         });
+        // get all review
         app.get('/review', async (req, res) => {
             const query = {};
             const reviews = await reviewCollection.find(query).toArray();
@@ -137,12 +142,14 @@ async function run() {
             res.send(result);
         });
 
+        // get all part
         app.post("/part", async (req, res) => {
             const product = req.body;
             const result = await partsCollection.insertOne(product);
             res.send(result);
         });
 
+        // get specific order
         app.get('/placeorder/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
@@ -157,7 +164,62 @@ async function run() {
             const result = await userCollection.deleteOne(query);
             res.send(result);
         })
+        // get user info
+        app.get('/userinfo/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            res.send(user);
+        })
 
+        // delete a product
+        app.delete('/part/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await partsCollection.deleteOne(query)
+            res.send(result);
+        })
+
+        // delete order 
+        app.delete('/placeorder/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await placeOrderCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.get("/placeorder/:id", verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await placeOrderCollection.findOne(query);
+            res.send(order);
+        });
+        app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+            const order = req.body;
+            const price = order.placeOrderPrice;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+            res.send({ clientSecret: paymentIntent.client_secret });
+        });
+        app.patch("/placeorder/:id", verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId,
+                },
+            };
+
+            const updatedOrder = await placeOrderCollection.updateOne(filter, updateDoc);
+            const result = await paymentCollection.insertOne(payment);
+            res.send(updateDoc);
+        });
     }
 
     finally {
